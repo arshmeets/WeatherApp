0aacc1eed2087bb0ff25525642696a03ce6f0a4e
Arshmeet Sodhi <arshmeetsodhi@gmail.com>
2021-03-27 12:32:07 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Arshmeet Sodhi on 3/27/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
